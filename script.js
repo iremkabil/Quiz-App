231283eccf4ab2eb2ac91417f44ae59fa063e64b
iremkabil <114577676+iremkabil@users.noreply.github.com>
2023-08-28 18:42:12 +0300

    Add Timer

--- script.js
+++ script.js
@@ -1,104 +1,111 @@
-//OOP: Nesne Tabanlı Programlama
-//Object
-
-//Sınıf, Constructor=> nesne * 30
-//ES5, ES6, ES7 -----tarayıcıda ES5 e cevrilip garantiye alinmasi lazim
-
-function  Soru(soruMetni, cevapSecenekleri, dogruCevap){
-    this.soruMetni = soruMetni;
-    this.cevapSecenekleri = cevapSecenekleri;
-    this.dogruCevap = dogruCevap;
-
-}
-
-Soru.prototype.cevabiKontrolEt = function(cevap){
-    return cevap === this.dogruCevap;
-}
-
-let sorular=[
-    new Soru("1-hangisi js paket yönetim uygulamasıdır?", { a:"Node.js", b:"Typescript", c:"Npm",d:"Nuget"}, "c"),
-
-    new Soru("2-hangisi .Net paket yönetim uygulamasıdır?", { a:"Node.js", b:"nuget", c:"Npm"}, "b"),
-    
-    new Soru("3-hangisi js paket yönetim uygulamasıdır?", { a:"Node.js", b:"Typescript", c:"Npm",d:"Nuget"}, "c"),
-
-    new Soru("4-hangisi .Net paket yönetim uygulamasıdır?", { a:"Node.js", b:"nuget", c:"Npm"}, "b")
-];
-
-function Quiz(sorular){
-    this.sorular = sorular;
-    this.soruIndex = 0;
-}
-
-Quiz.prototype.soruGetir = function(){
-    return this.sorular[this.soruIndex];
-}
-
 const quiz = new Quiz(sorular);
+const ui = new UI();
 
-document.querySelector(".btn_start").addEventListener("click",function(){
-    document.querySelector(".quiz_box").classList.add("active");
-    soruGoster(quiz.soruGetir());
-    document.querySelector(".next_btn").classList.remove("show");
+ui.btn_start.addEventListener("click",function(){
+    ui.quiz_box.classList.add("active");
+    startTimer(10);
+    startTimerLine();
+    ui.soruGoster(quiz.soruGetir());
+    ui.soruSayisiniGoster(quiz.soruIndex + 1, quiz.sorular.length);
+    ui.btn_next.classList.remove("show");
 
 })
-
-document.querySelector(".next_btn").addEventListener("click",function(){
+ 
+ui.btn_next.addEventListener("click",function(){
     if(quiz.sorular.length != quiz.soruIndex + 1){
         quiz.soruIndex += 1;
-        soruGoster(quiz.soruGetir());
-        document.querySelector(".next_btn").classList.remove("show");
+        clearInterval(counter);
+        startTimer(10);
+        clearInterval(counterLine);
+        startTimerLine();
+        ui.soruGoster(quiz.soruGetir());
+        ui.soruSayisiniGoster(quiz.soruIndex + 1, quiz.sorular.length);
+        ui.btn_next.classList.remove("show");
     }else{
-        console.log("quiz bitti");
+        clearInterval(counter);
+        ui.quiz_box.classList.remove("active");
+        ui.score_box.classList.add("active");
+        ui.skoruGoster(quiz.sorular.length, quiz.dogruCevapSayisi);
     }
 });
 
-const option_list = document.querySelector(".option_list");
-const correctIcon = '<div class="icon"><i class="fas fa-check"></i></div>';
-const incorrectIcon = '<div class="icon"><i class="fas fa-times"></i></div>';
-
-function soruGoster(soru){
-    let question = `<span>${soru.soruMetni}</span>`;
-    let options = '';
-
-    for(let cevap in soru.cevapSecenekleri){
-        options +=
-        `
-        <div class="option"> 
-            <span><b>${cevap}</b>:${soru.cevapSecenekleri[cevap]}</span>
-        </div>
-        `;
-    }
-    document.querySelector(".question_text").innerHTML = question;
-    option_list.innerHTML = options;
-
-    const option = option_list.querySelectorAll(".option");
-
-    for(let opt of option){
-        opt.setAttribute("onclick","optionSelected(this)")
-    }
-}
+ui.btn_quit.addEventListener("click", function(){
+    // sayfa tekrar yüklenir
+    window.location.reload();
+});
+ui.btn_replay.addEventListener("click", function(){
+    quiz.soruIndex = 0;
+    quiz.dogruCevapSayisi = 0;
+    ui.btn_start.click();
+    ui.score_box.classList.remove("active");
+});
 
 function optionSelected(option){
+    clearInterval(counter);
+    clearInterval(counterLine);
     let cevap = option.querySelector("span b").textContent;
     let soru = quiz.soruGetir();
 
     if(soru.cevabiKontrolEt(cevap)){
+        quiz.dogruCevapSayisi += 1;
         option.classList.add("correct");
-        option.insertAdjacentHTML("beforeend", correctIcon);
+        option.insertAdjacentHTML("beforeend", ui.correctIcon);
 
     }else{
         option.classList.add("incorrect");
-        option.insertAdjacentHTML("beforeend", incorrectIcon);
-
+        option.insertAdjacentHTML("beforeend", ui.incorrectIcon);
     }
 
-    for(let i=0; i<option_list.children.length; i++){
-        option_list.children[i].classList.add("disabled");
+    for(let i=0; i<ui.option_list.children.length; i++){
+        ui.option_list.children[i].classList.add("disabled");
     }
-    document.querySelector(".next_btn").classList.add("show");
+    ui.btn_next.classList.add("show");
 
 }
 
+let counter;
+
+function startTimer (time) {
+    // 1000 saniyede 1 bu metodu çağıracak
+    counter = setInterval(timer,1000);
+
+    function timer(){
+        ui.time_second.textContent = time;
+        time--;
+        if (time < 0){
+            // Bu metoda temizlenecek olan setIntervel'ın referansını vermiş oluyorsunuz yani bellekteki adresini vermiş oluyorsunuz
+            // Süre bittiğinde fonksiyonu durdurur
+            clearInterval(counter);
+            
+            ui.time_text.textContent = "Süre Bitti";
+            let cevap = quiz.soruGetir().dogruCevap;
+            for(let option of ui.option_list.children){
+                if(option.querySelector("span b").textContent == cevap) {
+                    option.classList.add("correct");
+                    option.insertAdjacentHTML("beforeend", ui.correctIcon);
+                }
+
+                option.classList.add("disabled");
+            }
+            ui.btn_next.classList.add("show");
+        }
+    }
+}
+
+let counterLine;
+function startTimerLine() {
+    let line_width = 0;
+
+    counterLine = setInterval (timer, 20);
+
+    function timer (){
+        line_width += 1;
+        ui.time_line.style.width = line_width +"px";
+
+        if(line_width > 549){
+            clearInterval(counterLine);
+        }
+    }
+}
 
 
